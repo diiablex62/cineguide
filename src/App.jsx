73784affd787f6128d59c7c82a52c856f4ceb7be
Aleexj <jalias59@outlook.fr>
2025-04-23 15:00:05 +0200

    Message

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { react } from "react";
 function App() {
   return (
     <h1 className='text-3xl font-bold underline text-red-700 text-center'>
-      Cinéguide
+      Cinéguide !
     </h1>
   );
 }
