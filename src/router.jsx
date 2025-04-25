605e0ef3b53d825a1c18bf3f97fa5f096f7ad113
Aleexj <jalias59@outlook.fr>
2025-04-25 09:32:56 +0200

    ajout routes

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import FilmList from "./pages/Films/FilmsList";
+import Connexion from "./pages/Auth/Connexion";
+import Inscription from "./pages/Auth/Inscription";
+
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -14,6 +17,14 @@ export const router = createBrowserRouter([
       {
         path: "/film",
         element: <FilmList />,
+      },
+      {
+        path: "/connexion",
+        element: <Connexion />,
+      },
+      {
+        path: "/inscription",
+        element: <Inscription />,
       },
     ],
   },
