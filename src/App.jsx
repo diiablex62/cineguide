b81c0c74afad77a1e9f7ccf753ae7a75db3352e4
Aleexj <jalias59@outlook.fr>
2025-04-25 08:55:00 +0200

    Ajout du contexte de thème et du fournisseur de thème pour la gestion dynamique du thème dans l'application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Inscription from "./pages/Auth/Inscription";
 import Connexion from "./pages/Auth/Connexion";
+import ThemeProvider from "./components/providers/ThemeProviders"; 
 
 function App() {
   return <Connexion />;
