0da1e7aa51df9778006f2fdf91ff3ce1fe783f09
[Koxtx] <corentin.lavarde@gmail.com>
2025-04-28 13:01:39 +0200

    detailserie

--- src/pages/DetailSerie/components/SerieProposer.jsx
+++ src/pages/DetailSerie/components/SerieProposer.jsx
@@ -19,10 +19,12 @@ export default function SerieProposer() {
     .sort((a, b) => b.note - a.note)
     .slice(0, 6);
 
-    // Obtenez des series spé (triés par note, excluant le serie actuel)
+  // Obtenez des series spé (triés par note, excluant le serie actuel)
   const dramaSeries = serie
-  .filter((serie) => serie.id !== detailSerie.id && serie.genre.includes("Drame"))
-  .slice(0, 6);
+    .filter(
+      (serie) => serie.id !== detailSerie.id && serie.genre.includes("Drame")
+    )
+    .slice(0, 6);
 
   return (
     <div className="md:w-2/3 flex-1">
@@ -32,28 +34,40 @@ export default function SerieProposer() {
         </h2>
         <div className="flex overflow-x-auto gap-4 pb-4">
           {similarSeries.slice(0, 4).map((serie) => (
-            <Card key={serie.id} serie={serie} currentSerieId={detailSerie.id} />
+            <Card
+              key={serie.id}
+              serie={serie}
+              currentSerieId={detailSerie.id}
+            />
           ))}
         </div>
 
         <div className="mt-12">
           <h2 className="font-bold mb-4 text-sm uppercase text-black dark:text-gray-400">
-            Films populaires
+            Séries populaires
           </h2>
           <div className="flex overflow-x-auto gap-4 pb-4">
             {popularSeries.map((serie) => (
-              <Card key={serie.id} serie={serie} currentSerieId={detailSerie.id} />
+              <Card
+                key={serie.id}
+                serie={serie}
+                currentSerieId={detailSerie.id}
+              />
             ))}
           </div>
         </div>
 
         <div className="mt-8">
           <h2 className="font-bold mb-4 text-sm uppercase text-black dark:text-gray-400">
-            Films Drame
+            Séries Drame
           </h2>
           <div className="flex overflow-x-auto gap-4 pb-4">
             {dramaSeries.map((serie) => (
-              <Card key={serie.id} serie={serie} currentSerieId={detailSerie.id} />
+              <Card
+                key={serie.id}
+                serie={serie}
+                currentSerieId={detailSerie.id}
+              />
             ))}
           </div>
         </div>
