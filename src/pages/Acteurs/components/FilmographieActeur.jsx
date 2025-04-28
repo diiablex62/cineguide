89d2bd2a1b0aacac9aca4b4fa117e3d9b4ba577f
BaptisteDev <61820472+BaptisteDev@users.noreply.github.com>
2025-04-28 19:10:46 +0200

    Ajout de filmographie plus la data

--- src/pages/Acteurs/components/FilmographieActeur.jsx
+++ src/pages/Acteurs/components/FilmographieActeur.jsx
@@ -1,62 +1,59 @@
 import React, { useContext } from "react";
 import { ActorContext } from "../../../context/ActorContext";
+import FilmParActeur from "../../../data/FilmParActeur.json";
 export default function FilmographieActeur() {
   const { actor } = useContext(ActorContext);
+
+  const filmsByActor = FilmParActeur.filter(
+    (film) => film.idActeur === actor.id
+  );
+
   return (
     <div className="w-full">
       <div>
-        <div className="flex justify-center md:justify-start">
-          <h2 className="font-bold text-2xl">Acteur</h2>
+        <div className="flex justify-center md:justify-start ">
+          <h2 className="font-bold text-2xl">Filmographie</h2>
         </div>
-        <div className=" ">
-          <div className="flex bg-gray-300 rounded-tl rounded-tr">
-            <p className="w-1/3 hidden md:block">Année</p>
-            <p className="w-1/3">Titre</p>
-            <p className="w-1/3 hidden md:block">Rôle</p>
+        <div>
+          <h2 className="mt-5 font-bold text-2xl">Acteur</h2>
+          <div className="flex items-center bg-gray-300 rounded-tl rounded-tr h-[50px] px-1 mt-4">
+            <p className="md:w-1/3 hidden md:block px-1">Année</p>
+            <p className="md:w-1/3 px-1">Titre</p>
+            <p className="md:w-1/3 hidden md:block px-1 text-right">Rôle</p>
           </div>
-          <div className="flex ">
-            <p className="w-1/3 hidden md:block">2028</p>
-            <p className="w-1/3">Oceanc’s 14</p>
-            <p className="w-1/3 hidden md:block">Tkt</p>
-          </div>
-          <div className="flex ">
-            <p className="w-1/3 hidden md:block">2028</p>
-            <p className="w-1/3">Oceanc’s 14</p>
-            <p className="w-1/3 hidden md:block">Tkt</p>
-          </div>
-          <div className="flex ">
-            <p className="w-1/3 hidden md:block">2028</p>
-            <p className="w-1/3">Oceanc’s 14</p>
-            <p className="w-1/3 hidden md:block">Tkt</p>
-          </div>
+          {filmsByActor.map((film) => (
+            <div key={film.id} className="flex items-center h-[50px]">
+              <p className="md:w-1/3 hidden md:block px-1">
+                {film.annee_sortie}
+              </p>
+              <p className="md:w-1/3 px-1">{film.titre}</p>
+              <p className="md:w-1/3 hidden md:block px-1 text-right">
+                {film.role}
+              </p>
+            </div>
+          ))}
         </div>
-      </div>
-      <div>
-        <div className="flex justify-center md:justify-start mt-4">
-          <h2 className="font-bold text-2xl">Production</h2>
-        </div>
-        <div className=" ">
-          <div className="flex bg-gray-300 rounded-tl rounded-tr">
-            <p className="w-1/3 hidden md:block">Année</p>
-            <p className="w-1/3">Titre</p>
-            <p className="w-1/3 hidden md:block">Rôle</p>
+        <div>
+          <h2 className="mt-5 font-bold text-2xl">Producteur</h2>
+          <div className="flex items-center bg-gray-300 rounded-tl rounded-tr h-[50px] px-1 mt-4">
+            <p className="md:w-1/3 hidden md:block px-1">Année</p>
+            <p className="md:w-1/3 px-1">Titre</p>
+            <p className="md:w-1/3 hidden md:block px-1 text-right">Rôle</p>
           </div>
-          <div className="flex ">
-            <p className="w-1/3 hidden md:block">2028</p>
-            <p className="w-1/3">Oceanc’s 14</p>
-            <p className="w-1/3 hidden md:block">Tkt</p>
-          </div>
-          <div className="flex ">
-            <p className="w-1/3 hidden md:block">2028</p>
-            <p className="w-1/3">Oceanc’s 14</p>
-            <p className="w-1/3 hidden md:block">Tkt</p>
-          </div>
-          <div className="flex ">
-            <p className="w-1/3 hidden md:block">2028</p>
-            <p className="w-1/3">Oceanc’s 14</p>
-            <p className="w-1/3 hidden md:block">Tkt</p>
-          </div>
-        </div>
+          {filmsByActor
+            .filter((film) => film.estProducteur)
+            .map((film) => (
+              <div key={film.id} className="flex items-center h-[50px]">
+                <p className="md:w-1/3 hidden md:block px-1">
+                  {film.annee_sortie}
+                </p>
+                <p className="md:w-1/3 px-1">{film.titre}</p>
+                <p className="md:w-1/3 hidden md:block px-1 text-right">
+                  {film.role}
+                </p>
+              </div>
+            ))}
+        </div>{" "}
       </div>
     </div>
   );
