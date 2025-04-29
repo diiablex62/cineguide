b1f8f046faaf2f55c9b578188dcb35205342b42e
Aleexj <jalias59@outlook.fr>
2025-04-29 13:47:31 +0200

    ajout de la version mobile pour la section Top 10 et ajustement de l'affichage sur desktop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,7 +39,7 @@ export default function Home() {
           Top 10 cette semaine &gt;
         </h2>
         <div className='relative w-[70%] mx-auto'>
-          <div className='flex gap-20 overflow-x-auto scroll-snap-x pl-10 pr-52 scrollbar-hide'>
+          <div className='hidden md:flex gap-20 overflow-x-auto scroll-snap-x pl-10 pr-52 scrollbar-hide'>
             {series.slice(0, 10).map((serie, index) => (
               <div
                 key={serie.id}
@@ -55,7 +55,32 @@ export default function Home() {
               </div>
             ))}
           </div>
-          <div className='absolute top-0 right-0 bottom-0 w-20 bg-gradient-to-l from-white via-white to-transparent dark:from-black dark:via-black pointer-events-none z-10'></div>
+          {/* Version mobile */}
+          <div className='md:hidden flex flex-col gap-4 -mx-10'>
+            {series.slice(0, 10).map((serie, index) => (
+              <div
+                key={serie.id}
+                className='flex h-24 bg-gray-200 dark:bg-gray-700'>
+                <div className='relative w-[20%]'>
+                  <div className='absolute bottom-[-15px] left-[-15px] text-4xl font-bold text-gray-800 dark:text-gray-400'>
+                    {index + 1}
+                  </div>
+                  <img
+                    src={serie.image}
+                    alt={serie.titre}
+                    className='h-full w-full object-cover'
+                  />
+                </div>
+                <div className='flex-1 p-4 flex items-center'>
+                  <h3 className='font-bold text-gray-800 dark:text-gray-200'>
+                    {serie.titre}
+                  </h3>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* Flou uniquement sur desktop */}
+          <div className='hidden md:block absolute top-0 right-0 bottom-0 w-20 bg-gradient-to-l from-white via-white to-transparent dark:from-black dark:via-black pointer-events-none z-10'></div>
         </div>
       </div>
     </div>
