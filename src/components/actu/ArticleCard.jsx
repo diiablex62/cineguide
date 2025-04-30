951c90a5e260b9c6240f3e02d3277dc06bfa4186
CorentinDWWM <newsonic62@gmail.com>
2025-04-30 14:04:15 +0200

    fix actu

--- src/components/actu/ArticleCard.jsx
+++ src/components/actu/ArticleCard.jsx
@@ -1,35 +1,30 @@
 import React from "react";
 
-export default function ArticleCard({
-  title,
-  image,
-  date,
-  category,
-  link,
-  onClick,
-}) {
+export default function ArticleCard({ title, image, date, category, onClick }) {
   return (
     <div
-      className='flex flex-col md:flex-row gap-4 mb-6 border-b border-gray-300 dark:border-white pb-4 w-full md:w-[50%] mx-auto cursor-pointer'
-      onClick={onClick}>
+      className="flex flex-col md:flex-row gap-4 mb-6 border-b border-gray-300 dark:border-white pb-4 w-full md:w-[50%] mx-auto cursor-pointer"
+      onClick={onClick}
+    >
       <img
         src={image}
         alt={title}
-        className='w-full md:w-1/2 h-48 object-cover rounded'
+        className="w-full md:w-1/2 h-48 object-cover rounded"
       />
-      <div className='flex flex-col justify-between md:w-1/2'>
+      <div className="flex flex-col justify-between md:w-1/2">
         <div>
-          <p className='text-sm text-[var(--color-fuchsia)] font-bold uppercase dark:text-white'>
+          <p className="text-sm text-[var(--color-fuchsia)] font-bold uppercase dark:text-white">
             {category} / {date}
           </p>
-          <h2 className='text-lg font-bold text-gray-800 dark:text-white'>
+          <h3 className="text-lg font-bold text-gray-800 dark:text-white">
             {title}
-          </h2>
+          </h3>
         </div>
         <a
-          href='#'
-          className='text-sm text-[var(--color-fuchsia)] hover:underline mt-2 dark:text-white'
-          onClick={(e) => e.stopPropagation()}>
+          href="#"
+          className="text-sm text-[var(--color-fuchsia)] hover:underline mt-2 dark:text-white"
+          onClick={(e) => e.stopPropagation()}
+        >
           Voir l'article complet
         </a>
       </div>
--- src/components/actu/FirstArticleCard.jsx
+++ src/components/actu/FirstArticleCard.jsx
@@ -25,31 +25,34 @@ export default function FirstArticleCard({
 
   return (
     <div
-      className='relative mb-8 w-full md:w-[50%] mx-auto cursor-pointer'
-      onClick={handleArticleClick}>
+      className="relative mb-8 w-full md:w-[50%] mx-auto cursor-pointer"
+      // onClick={handleArticleClick}
+    >
       <img
         alt={title}
-        className='w-full h-96 object-cover rounded-lg'
+        className="w-full h-96 object-cover rounded-lg"
         src={image}
       />
-      <div className='absolute inset-0 bg-opacity-50 rounded-lg flex flex-col justify-end p-6'>
-        <div className='bg-white/50 dark:bg-black/50 p-4 rounded-md'>
-          <p className='text-sm text-[var(--color-fuchsia)] font-bold uppercase'>
+      <div className="absolute inset-0 bg-opacity-50 rounded-lg flex flex-col justify-end p-6">
+        <div className="bg-white/50 dark:bg-black/50 p-4 rounded-md">
+          <p className="text-sm text-[var(--color-fuchsia)] font-bold uppercase">
             {category} / {date}
           </p>
-          <h2 className='text-2xl font-bold text-gray-800 dark:text-white mt-2'>
+          <h3 className="text-2xl font-bold text-gray-800 dark:text-white mt-2 max-sm:line-clamp-6">
             {title}
-          </h2>
-          <p className='text-sm text-gray-800 dark:text-white mt-2'>
+          </h3>
+          <p className="text-sm text-gray-800 dark:text-white mt-2">
             {description}
           </p>
           <a
             href={link}
-            className='text-sm text-[var(--color-fuchsia)] hover:underline mt-4 block'
+            target="_blank"
+            className="text-sm text-[var(--color-fuchsia)] hover:underline mt-4 block"
             onClick={(e) => {
               e.stopPropagation();
               handleClick();
-            }}>
+            }}
+          >
             Voir l'article complet
           </a>
         </div>
