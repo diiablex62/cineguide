23a8c97304fe3db4ed89644bbff5a5339b0c8c65
CorentinDWWM <newsonic62@gmail.com>
2025-04-30 14:23:26 +0200

    fix

--- src/pages/Series/SeriesList.jsx
+++ src/pages/Series/SeriesList.jsx
@@ -4,18 +4,18 @@ import CardSerie from "./components/CardSerie";
 
 export default function SeriesList() {
   return (
-     <div className="dark:bg-black dark:text-white min-h-screen">
-          <div className="md:flex md:p-4">
-            <div className="md:w-[40%] md:mr-5 lg:w-[25%] ">
-              <Filtre></Filtre>
-            </div>
-            <div className="md:w-[60%] lg:w-[75%]">
-              <CardSerie></CardSerie>
-            </div>
-          </div>
-          {/* <div className="flex justify-center mt-5">
+    <div className="dark:bg-black dark:text-white min-h-screen mt-5">
+      <div className="md:flex md:p-4">
+        <div className="md:w-[40%] md:mr-5 lg:w-[25%] ">
+          <Filtre></Filtre>
+        </div>
+        <div className="md:w-[60%] lg:w-[75%]">
+          <CardSerie></CardSerie>
+        </div>
+      </div>
+      {/* <div className="flex justify-center mt-5">
             <button className="cursor-pointer">Voir plus</button>
           </div> */}
-        </div>
+    </div>
   );
 }
