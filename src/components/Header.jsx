9e6795761cca05dbbb714efedb1c27abe488c1f1
Aleexj <jalias59@outlook.fr>
2025-04-30 11:48:02 +0200

    chnagement lien flag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,56 +27,51 @@ export default function Header() {
   const { logout, isLoggedIn } = useContext(AuthContext);
 
   return (
-    <div className="flex items-center justify-between shadow-md p-4  gap-2.5 dark:bg-black dark:text-white dark:shadow-white">
+    <div className='flex items-center justify-between shadow-md p-4  gap-2.5 dark:bg-black dark:text-white dark:shadow-white'>
       {theme === "dark" ? (
-        <Link to="/">
-          <img src={logoWhite} alt="logo blanc cineguide" className="w-52" />
+        <Link to='/'>
+          <img src={logoWhite} alt='logo blanc cineguide' className='w-52' />
         </Link>
       ) : (
-        <Link to="/">
-          <img src={logo} alt="logo cineguide" className="w-52" />
+        <Link to='/'>
+          <img src={logo} alt='logo cineguide' className='w-52' />
         </Link>
       )}
-      <div className="flex items-center flex-col gap-4 max-1100:hidden">
-        <div className="flex justify-between items-center max-1500:w-[400px] w-[640px] border border-black p-2 dark:border-white">
+      <div className='flex items-center flex-col gap-4 max-1100:hidden'>
+        <div className='flex justify-between items-center max-1500:w-[400px] w-[640px] border border-black p-2 dark:border-white'>
           <input
-            type="text"
-            placeholder="Rechercher un film, une série, un acteur ..."
-            className="w-full outline-none placeholder:text-gray-500 dark:placeholder:text-white"
+            type='text'
+            placeholder='Rechercher un film, une série, un acteur ...'
+            className='w-full outline-none placeholder:text-gray-500 dark:placeholder:text-white'
           />
-          <IoSearchOutline className="dark:text-white" />
+          <IoSearchOutline className='dark:text-white' />
         </div>
-        <div className="w-full flex">
+        <div className='w-full flex'>
           <NavLink
-            to="/"
-            className="mr-5 text-black  dark:text-white hover:text-fuchsia"
-          >
+            to='/'
+            className='mr-5 text-black  dark:text-white hover:text-fuchsia'>
             Accueil
           </NavLink>
           <NavLink
-            to="/film"
-            className="mr-5 text-black  dark:text-white hover:text-fuchsia"
-          >
+            to='/film'
+            className='mr-5 text-black  dark:text-white hover:text-fuchsia'>
             Films
           </NavLink>
           <NavLink
-            to="/series"
-            className="mr-5 text-black  dark:text-white hover:text-fuchsia"
-          >
+            to='/series'
+            className='mr-5 text-black  dark:text-white hover:text-fuchsia'>
             Séries
           </NavLink>
           <NavLink
-            to="/actualites"
-            className="mr-5 text-black  dark:text-white hover:text-fuchsia"
-          >
+            to='/actualites'
+            className='mr-5 text-black  dark:text-white hover:text-fuchsia'>
             Actualités
           </NavLink>
           {menu ? (
             <div>
               <div
                 onClick={() => setMenu(false)}
-                className="flex items-center gap-1 hover:text-fuchsia cursor-pointer text-black dark:text-white"
-              >
+                className='flex items-center gap-1 hover:text-fuchsia cursor-pointer text-black dark:text-white'>
                 <p>Plus</p>
                 <FaChevronUp />
               </div>
@@ -85,74 +80,67 @@ export default function Header() {
           ) : (
             <div
               onClick={() => setMenu(true)}
-              className="flex items-center gap-1 hover:text-fuchsia cursor-pointer text-black dark:text-white"
-            >
+              className='flex items-center gap-1 hover:text-fuchsia cursor-pointer text-black dark:text-white'>
               <p>Plus</p>
               <FaChevronDown />
             </div>
           )}
         </div>
       </div>
-      <div className="flex flex-col gap-4 items-center justify-end max-1100:hidden">
-        <div className="flex items-center gap-5">
+      <div className='flex flex-col gap-4 items-center justify-end max-1100:hidden'>
+        <div className='flex items-center gap-5'>
           {isLoggedIn ? (
             <>
               <NavLink
-                to="/profil"
-                className="bg-fuchsia flex justify-center items-center h-[50px] px-2 text-white "
-              >
+                to='/profil'
+                className='bg-fuchsia flex justify-center items-center h-[50px] px-2 text-white '>
                 Mon compte
               </NavLink>
               <a
                 onClick={logout}
-                className="text-2xl cursor-pointer hover:text-fuchsia"
-              >
+                className='text-2xl cursor-pointer hover:text-fuchsia'>
                 <IoIosLogOut />
               </a>
             </>
           ) : (
             <>
               <NavLink
-                to="/connexion"
-                className="bg-fuchsia flex justify-center items-center h-[50px] w-[150px] px-2 text-white "
-              >
+                to='/connexion'
+                className='bg-fuchsia flex justify-center items-center h-[50px] w-[150px] px-2 text-white '>
                 Connexion
               </NavLink>
               <NavLink
-                to="/inscription"
-                className="bg-white text-black flex justify-center items-center dark:bg-black dark:text-white h-[50px] w-[150px] border max-1500:px-8"
-              >
+                to='/inscription'
+                className='bg-white text-black flex justify-center items-center dark:bg-black dark:text-white h-[50px] w-[150px] border max-1500:px-8'>
                 S'inscrire
               </NavLink>
             </>
           )}
           <div
             onClick={() => setLangageMenu(true)}
-            className="flex cursor-pointer  items-center justify-between relative px-4 py-2.5 gap-2 w-fit border bg-white dark:bg-black dark:border-white"
-          >
-            <img src={fr} alt="drapeau langue française" className="w-8" />
+            className='flex cursor-pointer  items-center justify-between relative px-4 py-2.5 gap-2 w-fit border bg-white dark:bg-black dark:border-white'>
+            <img src={fr} alt='drapeau langue française' className='w-8' />
             <FaChevronDown />
           </div>
           {langageMenu && <MenuLangage />}
         </div>
-        <div className="flex gap-[30px] items-center justify-end w-full">
+        <div className='flex gap-[30px] items-center justify-end w-full'>
           <div
             onClick={toggleTheme}
-            className="flex items-center gap-8 justify-center cursor-pointer"
-          >
+            className='flex items-center gap-8 justify-center cursor-pointer'>
             {theme === "dark" ? (
               <>
-                <MdSunny className="text-3xl" />
+                <MdSunny className='text-3xl' />
               </>
             ) : (
               <>
-                <BsFillMoonStarsFill className="text-3xl" />
+                <BsFillMoonStarsFill className='text-3xl' />
               </>
             )}
           </div>
-          <div className="flex gap-5 items-center justify-center text-3xl">
-            <FaFacebookSquare className="dark:text-white" />
-            <FaSquareXTwitter className="dark:text-white" />
+          <div className='flex gap-5 items-center justify-center text-3xl'>
+            <FaFacebookSquare className='dark:text-white' />
+            <FaSquareXTwitter className='dark:text-white' />
           </div>
         </div>
       </div>
@@ -162,7 +150,7 @@ export default function Header() {
       ) : (
         <RiMenu2Fill
           onClick={toggleBurger}
-          className="hidden max-1100:block text-3xl cursor-pointer"
+          className='hidden max-1100:block text-3xl cursor-pointer'
         />
       )}
     </div>
--- src/components/providers/LangageProvider.jsx
+++ src/components/providers/LangageProvider.jsx
@@ -1,40 +1,39 @@
 import React, { useEffect, useRef, useState } from "react";
 import { LangageContext } from "../../context/LangageContext";
+import fr from "../../assets/france.png";
+import uk from "../../assets/royaume_uni.png";
+import es from "../../assets/espagne.png";
+import de from "../../assets/allemagne.png";
 
 export default function LangageProvider({ children }) {
   const [langage, setLangage] = useState([
     {
       id: 1,
       langue: "fr",
-      img: "/src/assets/france.png",
+      img: fr,
       desc: "drapeau langue française",
     },
     {
       id: 2,
       langue: "en",
-      img: "/src/assets/royaume_uni.png",
+      img: uk,
       desc: "drapeau langue anglaise",
     },
     {
       id: 3,
       langue: "es",
-      img: "/src/assets/espagne.png",
+      img: es,
       desc: "drapeau langue espagnol",
     },
     {
       id: 4,
       langue: "de",
-      img: "/src/assets/allemagne.png",
+      img: de,
       desc: "drapeau langue allemande",
     },
   ]);
   const [langageMenu, setLangageMenu] = useState(false);
 
-  //   const toggleLangage = () => {
-  //     setLangageMenu();
-  //   };
-
-  //   const [menu, setMenu] = useState(false);
   const langageMenuRef = useRef(null);
 
   useEffect(() => {
@@ -60,8 +59,7 @@ export default function LangageProvider({ children }) {
 
   return (
     <LangageContext.Provider
-      value={{ langage, langageMenu, setLangageMenu, langageMenuRef }}
-    >
+      value={{ langage, langageMenu, setLangageMenu, langageMenuRef }}>
       {children}
     </LangageContext.Provider>
   );
