7e14705c30a2d1258260c568a98601350ad2e2d1
CorentinDWWM <newsonic62@gmail.com>
2025-04-30 10:54:26 +0200

    fix menu langage

--- src/components/menu/menuLangage/MenuLangage.jsx
+++ src/components/menu/menuLangage/MenuLangage.jsx
@@ -9,13 +9,13 @@ export default function MenuLangage() {
     // <div className="relative">
     <div
       ref={langageMenuRef}
-      className="min-1100:absolute min-1100:mt-[172px] cursor-pointer z-50 min-1100:ml-[182px] bg-white border shadow-md dark:shadow-white dark:bg-black"
+      className="min-1100:absolute min-1100:mt-[156px] cursor-pointer z-50 min-1100:ml-[340px] bg-white border shadow-md dark:shadow-white dark:bg-black"
     >
       {langage.map((l) => (
         <div
           onClick={() => setLangageMenu(false)}
           key={l.id}
-          className="flex items-center cursor-pointer justify-between px-4 py-3 max-1100:py-2.5 gap-2 w-[88px] bg-white dark:bg-black dark:border-white"
+          className="flex items-center cursor-pointer justify-between px-4 py-2.5 max-1100:py-2.5 gap-2 w-fit bg-white dark:bg-black dark:border-white"
         >
           <img src={l.img} alt={l.desc} className="w-8" />
           {l.id === 1 ? <FaChevronUp /> : null}
