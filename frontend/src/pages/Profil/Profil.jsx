9df72e574885b0a3be7f7c5a2efc4f2da76675e8
BaptisteDev <dev.baptiste15@gmail.com>
2025-05-15 13:25:08 +0200

    update profil

--- frontend/src/pages/Profil/Profil.jsx
+++ frontend/src/pages/Profil/Profil.jsx
@@ -17,17 +17,19 @@ export default function profil() {
           location.pathname === "/profil"
             ? "flex flex-col p-5 md:flex-row dark:bg-black dark:text-white dark:border-white"
             : "flex flex-col p-5 dark:bg-black dark:text-white dark:border-white"
-        }>
+        }
+      >
         <div
           className={
             location.pathname === "/profil"
               ? "md:w-4/6 flex flex-col p-4"
               : "md:w-auto flex flex-col p-4"
-          }>
+          }
+        >
           <ProfilNav></ProfilNav>
           <ProfilForm></ProfilForm>
         </div>
-        <div className='w-full md:w-2/6'>
+        <div className="w-full md:w-2/6">
           <ProfilUtils></ProfilUtils>
         </div>
       </div>
