de0540780564fd736874f9b0913ba9d8b86103f0
Aleexj <jalias59@outlook.fr>
2025-05-19 10:24:09 +0200

    Suppression de la classe de fond noire dans le composant Jeux pour améliorer la lisibilité.

--- frontend/src/pages/Jeux/Jeux.jsx
+++ frontend/src/pages/Jeux/Jeux.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 const Jeux = () => {
   return (
-    <div className='bg-black text-white min-h-screen'>
+    <div className=' text-white min-h-screen'>
       <div className='flex flex-col lg:flex-row mb-5 lg:justify-between container w-full rounded'>
         <div className='w-full lg:w-2/8 bg-white dark:bg-black mt-5 shadow rounded lg:rounded-l-xl lg:mr-2 text-black dark:text-white'>
           <Navbar></Navbar>
