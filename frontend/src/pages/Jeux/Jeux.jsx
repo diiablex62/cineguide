de358ad58b669fa24f6753471b530b6c3f288656
Aleexj <jalias59@outlook.fr>
2025-05-22 11:10:00 +0200

    feat: enhance Jeux page layout and add welcome message for homepage

--- frontend/src/pages/Jeux/Jeux.jsx
+++ frontend/src/pages/Jeux/Jeux.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 const Jeux = () => {
+  const location = useLocation();
+  const isAccueil = location.pathname === "/jeux";
+
   return (
-    <div className=' text-white min-h-screen'>
-      <div className='flex flex-col lg:flex-row mb-5 lg:justify-between container w-full rounded'>
-        <div className='w-full lg:w-2/8 bg-white dark:bg-black mt-5 shadow rounded lg:rounded-l-xl lg:mr-2 text-black dark:text-white'>
-          <Navbar></Navbar>
-        </div>
-        <div className='w-full lg:w-6/8 lg:flex lg:flex-col lg:items-center px-10 bg-white dark:bg-black mt-5 shadow rounded lg:rounded-r-xl min-h-screen text-black dark:text-white'>
+    <div className='flex flex-col lg:flex-row mb-5 lg:justify-between container w-full rounded min-h-screen text-black bg-gray-300 rounded-l p-5'>
+      <div className='w-full lg:w-2/8 bg-white mt-5 shadow rounded lg:rounded-l-xl lg:mr-2'>
+        <Navbar />
+      </div>
+      <div className='w-full lg:w-6/8 lg:flex lg:flex-col text-black lg:items-center px-10 bg-white mt-5 shadow rounded lg:rounded-r-xl h-auto flex items-center justify-center'>
+        {isAccueil ? (
+          <div className='w-full h-full px-10 py-8'>
+            <h1 className='text-3xl font-bold mb-4'>
+              Bienvenue sur Cineguide Jeux !
+            </h1>
+            <p className='text-lg'>
+              Choisissez un jeu dans le menu à gauche pour commencer à jouer.
+            </p>
+          </div>
+        ) : (
           <Outlet />
-        </div>
+        )}
       </div>
     </div>
   );
